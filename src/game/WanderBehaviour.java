2ab2482a6aead66bd65d1a49f61e67c705ace51c
tongtie <lhuu0017@student.monash.edu>
2025-04-25 19:22:29 +0800

    Created WanderBehaviour to enable random movement for NPCs using MoveActorAction

---
+++ src/game/WanderBehaviour.java
@@ -0,0 +1,36 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.MoveActorAction;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.Behaviour;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+import java.util.*;
+
+/**
+ * A behaviour that makes an actor wander randomly.
+ */
+public class WanderBehaviour implements Behaviour {
+    /**
+     * Returns a move action to a random valid location.
+     * @param actor The actor performing the behaviour
+     * @param map   The game map
+     * @return A move action or null if no valid move exists
+     */
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        List<Exit> exits = new ArrayList<>(map.locationOf(actor).getExits());
+        Collections.shuffle(exits);
+        for (Exit exit : exits) {
+            Location destination = exit.getDestination();
+            if (!destination.containsAnActor() && destination.getGround().canActorEnter(actor)) {
+                return new MoveActorAction(destination, exit.getName());
+            }
+        }
+        return null;
+    }
+}
