6c4d3c485c65e83812c3613be0d1e41e7fc6c7a9
tongtie <lhuu0017@student.monash.edu>
2025-04-25 19:21:25 +0800

    Updated Player to add stamina attribute using BaseActorAttributes.STAMINA and display HP/stamina each turn, Refined Player playTurn to add AttackAction only for non-hostile nearby actors

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,37 +1,43 @@
 package game;
 
+import edu.monash.fit2099.engine.*;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttribute;
 import edu.monash.fit2099.engine.displays.Display;
-import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.displays.Menu;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
 
 /**
- * Class representing the Player.
+ * Class representing the Player (Farmer).
  * @author Adrian Kristanto
  */
 public class Player extends Actor {
-    /**
-     * Constructor.
-     *
-     * @param name        Name to call the player in the UI
-     * @param displayChar Character to represent the player in the UI
-     * @param hitPoints   Player's starting number of hitpoints
-     */
     public Player(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
         this.addCapability(Status.HOSTILE_TO_ENEMY);
         this.setIntrinsicWeapon(new BareFist());
+        this.addAttribute(BaseActorAttributes.STAMINA, new BaseActorAttribute(200));
     }
 
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        // Handle multi-turn Actions
-        if (lastAction.getNextAction() != null)
+        display.println(this + ", Stamina=" + getAttribute(BaseActorAttributes.STAMINA) + "/" + getAttributeMaximum(BaseActorAttributes.STAMINA));
+        if (lastAction.getNextAction() != null) {
             return lastAction.getNextAction();
+        }
 
-        // return/print the console menu
+        for (Exit exit : map.locationOf(this).getExits()) {
+            Location destination = exit.getDestination();
+            if (destination.containsAnActor() && !destination.getActor().hasCapability(Status.HOSTILE_TO_ENEMY)) {
+                actions.add(new AttackAction(destination.getActor()));
+            }
+        }
+
         Menu menu = new Menu(actions);
         return menu.showMenu(this, display);
     }
