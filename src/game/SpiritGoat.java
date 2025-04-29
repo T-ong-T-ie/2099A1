ab8a80fc3745efe288cfbe38b5fe481f36f53718
tongtie <lhuu0017@student.monash.edu>
2025-04-30 00:08:58 +0800

    Delete unused import statements

--- src/game/SpiritGoat.java
+++ src/game/SpiritGoat.java
@@ -3,7 +3,6 @@ package game;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 
