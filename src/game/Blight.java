e622b2aa7f5de19b893305894a17b9da87cd1bb0
tongtie <lhuu0017@student.monash.edu>
2025-04-29 23:49:12 +0800

    added the ability to heal the land with talisman

--- src/game/Blight.java
+++ src/game/Blight.java
@@ -6,8 +6,18 @@ import edu.monash.fit2099.engine.positions.Ground;
  * A class representing a blight covering the ground of the valley.
  * @author Adrian Kristanto
  */
-public class Blight extends Ground {
+public class Blight extends Ground implements CurableGround {
     public Blight() {
         super('x', "Blight");
+    }
+
+    @Override
+    public Ground cure() {
+        return new Soil();
+    }
+
+    @Override
+    public boolean isCurable() {
+        return true;
     }
 }
