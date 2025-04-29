81b797e21954fa28c8ac829230b1a5794e8b1a94
tongtie <lhuu0017@student.monash.edu>
2025-04-29 19:42:54 +0800

    delete invalid import

--- src/game/SpiritGoat.java
+++ src/game/SpiritGoat.java
@@ -1,6 +1,5 @@
 package game;
 
-import edu.monash.fit2099.engine.*;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
