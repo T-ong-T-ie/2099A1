190c5401e7d711949b8550c8def288200974e2e8
tongtie <lhuu0017@student.monash.edu>
2025-04-29 21:42:11 +0800

    添加玩家攻击的耐力消耗

--- src/game/AttackAction.java
+++ src/game/AttackAction.java
@@ -1,26 +1,16 @@
 package game;
 
-import java.util.Random;
-
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.GameMap;
-
 
 /**
  * Special Action for attacking other Actors.
  */
 public class AttackAction extends Action {
 
-    /**
-     * The Actor that is to be attacked
-     */
-    private Actor target;
-
-    /**
-     * Random number generator
-     */
-    private Random rand = new Random();
+    private final Actor target;
 
     /**
      * Constructor.
@@ -32,40 +22,48 @@ public class AttackAction extends Action {
     }
 
     /**
-     * Execute the attack action.
+     * Perform the attack action.
      *
-     * @param actor The actor performing the action.
-     * @param map   The map the actor is on.
-     * @return a description of what happened that can be displayed to the user.
+     * @param actor The actor performing the action
+     * @param map The map the actor is on
+     * @return a description of what happened that can be displayed to the user
      */
     @Override
     public String execute(Actor actor, GameMap map) {
         if (actor instanceof Player) {
-            // Player can have an equipped weapon or use intrinsic weapon
             Player player = (Player) actor;
-            WeaponItem equippedWeapon = player.getEquippedWeapon();
 
-            // Apply stamina cost for attack (this method needs to be added)
+            // Check if player has enough stamina to attack (minimum 15)
+            if (player.getAttribute(BaseActorAttributes.STAMINA) < 15) {
+                return actor + " is too tired to attack!";
+            }
+
+            // Apply stamina cost for attack
             player.applyAttackStaminaCost();
 
+            WeaponItem equippedWeapon = player.getEquippedWeapon();
             if (equippedWeapon != null) {
-                // Use equipped weapon if available
                 return equippedWeapon.attack(actor, target, map);
             } else {
-                // Use intrinsic weapon otherwise
                 return actor.getIntrinsicWeapon().attack(actor, target, map);
             }
         } else {
-            // Non-player actors just use their intrinsic weapon
-            return actor.getIntrinsicWeapon().attack(actor, target, map);
+            // Non-player actors don't have stamina restrictions
+            if (actor.getIntrinsicWeapon() != null) {
+                return actor.getIntrinsicWeapon().attack(actor, target, map);
+            } else {
+                // Fallback to a basic attack if no intrinsic weapon
+                target.hurt(5);
+                return actor + " attacks " + target + " for 5 damage";
+            }
         }
     }
 
     /**
-     * Returns a descriptive string for the menu.
+     * Returns a descriptive string for the action menu
      *
-     * @param actor The actor performing the action.
-     * @return a String describing the action, suitable for displaying in the menu.
+     * @param actor The actor performing the action
+     * @return a description used for the menu
      */
     @Override
     public String menuDescription(Actor actor) {
