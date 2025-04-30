e4f304593a39802dd6889b17fea951badaaab837
tongtie <lhuu0017@student.monash.edu>
2025-04-30 20:05:20 +0800

    Remove the code for flowers to check death status, as it has been implemented in player

--- src/game/BloodroseSeed.java
+++ src/game/BloodroseSeed.java
@@ -1,8 +1,6 @@
 package game;
 
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
-import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.Location;
 
 /**
@@ -22,16 +20,6 @@ public class BloodroseSeed extends Seed {
         // Plant the Bloodrose
         location.addItem(new Bloodrose());
 
-        // Damage the farmer when planting
-        actor.hurt(5);
-
-        String result = actor + " planted a Bloodrose, taking 5 damage";
-
-        // Check if the farmer died from planting
-        if (!actor.isConscious()) {
-            result += "\nYOU DIED";
-        }
-
-        return result;
+        return actor + " planted a Bloodrose";
     }
 }
