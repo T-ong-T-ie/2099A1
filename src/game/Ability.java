d8cee21f81aeb3f6dcfe3834af86dc25770fe36f
tongtie <lhuu0017@student.monash.edu>
2025-04-30 20:31:53 +0800

    Simplified comments

--- src/game/Ability.java
+++ src/game/Ability.java
@@ -2,7 +2,6 @@ package game;
 
 /**
  * Use this enum to represent abilities.
- * Example #1: if the player is capable jumping over walls, you can attach Ability.WALL_JUMP to the player class
  */
 public enum Ability {
 }
